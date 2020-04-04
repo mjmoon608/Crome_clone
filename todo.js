4205fe698f89482f963e06e6eba47ab13984ff3c
DP_Bell <smaj664@gmail.com>
2020-04-05 03:49:14 +0900

    JSON.stringfy(javascript object 객체); : 자바스크립트 object를 string으로 변경 array.forEach(function(name){함수 동작 내용});:파라미터에 array에 담겨있는 것들이 각각 한번씩 들어가게됨 따로 함수를 만들어서 사용 할 수도 있음 -> todo list 로컬저장

--- todo.js
+++ todo.js
@@ -4,17 +4,33 @@ const toDoForm = document.querySelector(".js-toDoForm"),
 
 const TODOS_LS = "toDos";
 
+const toDos = [];
+
+function saveToDos(){
+    localStorage.setItem(TODOS_LS, JSON.stringify(toDos));
+}
+
 function paintToDo(text){
     const li = document.createElement("li");
     const delBtn = document.createElement("button");
+    const span = document.createElement("span");
+    const newId = toDos.length + 1
 
     delBtn.innerText = "❌";
     
-    const span = document.createElement("span");
     span.innerText = text;
     li.appendChild(span);
     li.appendChild(delBtn);
+    li.id = newId;
     toDoList.appendChild(li);
+    
+    const toDoObj = {
+        text: text,
+        id: newId
+    };
+
+    toDos.push(toDoObj);
+    saveToDos();
 }
 
 function handleSubmit(event){
@@ -25,10 +41,13 @@ function handleSubmit(event){
 }
 
 function loadToDos(){
-    const toDos = localStorage.getItem(TODOS_LS);
-    
-    if (toDos !== null){
-            
+    const loadedtoDos = localStorage.getItem(TODOS_LS);
+
+    if (loadedtoDos !== null){
+        const parsedToDos = JSON.parse(loadedtoDos);
+        parsedToDos.forEach(function(toDo){
+           paintToDo(toDo.text);
+        });
     }
 }
 
