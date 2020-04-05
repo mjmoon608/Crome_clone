90f5787e04c6faaabb0384fe5d18e183982e967d
DP_Bell <smaj664@gmail.com>
2020-04-05 21:59:01 +0900

    list.filter()/list.forEach() : list에 있는 모든 item을 위한 함수를 실행하는것 -> todo list 로컬저장 항목 제거

--- todo.js
+++ todo.js
@@ -4,7 +4,18 @@ const toDoForm = document.querySelector(".js-toDoForm"),
 
 const TODOS_LS = "toDos";
 
-const toDos = [];
+let toDos = [];
+
+function deleteToDo(event){
+    const btn = event.target;
+    const li = btn.parentNode;
+    toDoList.removeChild(li);
+    const cleanToDos = toDos.filter(function(toDo){
+        return toDo.id !== parseInt(li.id);
+    });
+    toDos = cleanToDos;
+    saveToDos();
+}
 
 function saveToDos(){
     localStorage.setItem(TODOS_LS, JSON.stringify(toDos));
@@ -17,10 +28,11 @@ function paintToDo(text){
     const newId = toDos.length + 1
 
     delBtn.innerText = "❌";
+    delBtn.addEventListener("click", deleteToDo);
     
     span.innerText = text;
-    li.appendChild(span);
     li.appendChild(delBtn);
+    li.appendChild(span);
     li.id = newId;
     toDoList.appendChild(li);
     
@@ -42,9 +54,10 @@ function handleSubmit(event){
 
 function loadToDos(){
     const loadedtoDos = localStorage.getItem(TODOS_LS);
-
+    
     if (loadedtoDos !== null){
         const parsedToDos = JSON.parse(loadedtoDos);
+
         parsedToDos.forEach(function(toDo){
            paintToDo(toDo.text);
         });
