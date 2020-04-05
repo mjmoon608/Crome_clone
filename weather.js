78be1a5eb82035d7930cca5b7296dcbb4dc41182
DP_Bell <smaj664@gmail.com>
2020-04-06 01:11:37 +0900

    api불러오기 -> fetch{}.then() 기본 사용법 -> geolocation을 이용해 latitude, longitude불러온 값으로 getweather API 사용  날씨정보, 지역이름 불러오기 구현

--- weather.js
+++ weather.js
@@ -1,7 +1,18 @@
+const weather = document.querySelector(".js-weather");
+
 const API_KEY = "7bb28c9272b214b71c05aa0c22c592b4";
 const COORDS = "coords";
 
-
+function getWeather(latitude, longitude){
+    fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=${API_KEY}&units=metric`
+    ).then(function(response){
+        return response.json();
+    }).then(function(json){
+        const temperature = json.main.temp;
+        const place = json.name;
+        weather.innerText = `${temperature} @ ${place}`;
+    });    
+}
 function saveCoords(coordsObj){
     localStorage.setItem(COORDS, JSON.stringify(coordsObj));
 }
@@ -18,6 +29,7 @@ function handleGeoSuccess(position){
         longitude
     };
     saveCoords(coordsObj);
+    getWeather(latitude,longitude);
 }
 
 function handleGeoError(){
@@ -33,7 +45,8 @@ function loadCoords(){
     if(loadedCoords === null){
         askForCoords();
     } else{
-        //getWeather
+        const parseCoords = JSON.parse(loadedCoords);
+        getWeather(parseCoords.latitude, parseCoords.longitude);
     }
 }
 
